bfea0f0a952a3b3574a5de05026f66041f1d22ba
yqbjtu <yqbjtu@163.com>
2018-03-27 19:33:35 +0800

    fix test defect

--- src/test/java/com/yq/demo/controller/ApplicationTests.java
+++ src/test/java/com/yq/demo/controller/ApplicationTests.java
@@ -1,8 +1,5 @@
 package com.yq.demo.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,7 +20,7 @@ public class ApplicationTests {
 
     @Test
     public void accessUnsecuredResourceThenOk() throws Exception {
-        mockMvc.perform(get("/"))
+        mockMvc.perform(get("/hello"))
             .andExpect(status().isOk());
     }
 }
